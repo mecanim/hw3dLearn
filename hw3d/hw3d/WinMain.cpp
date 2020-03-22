ef9d12059289cb10b8cd335600cec4b6f9bec60f
mecanim <mecanim@mail.ru>
2020-03-22 21:07:03 +0300

    Added message loop

--- hw3d/hw3d/WinMain.cpp
+++ hw3d/hw3d/WinMain.cpp
@@ -1,14 +1,44 @@
 #include <Windows.h>
+#include "WindowsMessageMap.h"
 
 const auto pClassName = "Hw3dClass";
 const auto pWindowName = "Direct3D window";
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	static WindowsMessageMap mm;
+	OutputDebugString(mm(msg, lParam, wParam).c_str());
+
+	switch (msg)
+	{
+	case WM_CLOSE:
+		PostQuitMessage(69);
+		break;
+	case WM_KEYDOWN:
+		if (wParam == 'F')
+		{
+			SetWindowText(hWnd, "New title");
+		}
+		break;
+	case WM_KEYUP:
+		if (wParam = 'F')
+		{
+			SetWindowText(hWnd, pWindowName);
+		}
+		break;
+	default:
+		break;
+	}
+
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
 
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 	WNDCLASSEX wc = { 0 };
 	wc.cbSize = sizeof(wc);
 	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = DefWindowProc;
+	wc.lpfnWndProc = WndProc;
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
 	wc.hInstance = hInstance;
@@ -30,10 +60,21 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 
 	ShowWindow(hWnd, nCmdShow);
 
-	while (true)
+	MSG msg;
+	BOOL gResult;
+	while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
 	{
-
+		TranslateMessage(&msg);
+		//Pass the message along to the window proc
+		DispatchMessage(&msg);
 	}
 
-	return 0;
+	if (gResult == -1)
+	{
+		return -1;
+	}
+	else
+	{
+		return msg.wParam;
+	}
 }
