824ab1b221605834674118190d6bd069fccb2a60
mecanim <laborotory@mail.ru>
2020-08-24 18:09:49 +0300

    Added vertex and pixel shaders. Triangle showed.

--- hw3d/hw3d/Graphics.cpp
+++ hw3d/hw3d/Graphics.cpp
@@ -1,6 +1,11 @@
 #include "Graphics.h"
+#include <sstream>
+#include <d3dcompiler.h>
+
+namespace wrl = Microsoft::WRL;
 
 #pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "D3DCompiler.lib")
 
 Graphics::Graphics(HWND hWnd)
 {
@@ -36,10 +41,12 @@ Graphics::Graphics(HWND hWnd)
 		&pContext
 	);
 
-	ID3D11Resource* pBackBuffer = nullptr;
-	pSwap->GetBuffer(0, __uuidof(ID3D11Resource), reinterpret_cast<void**>(&pBackBuffer));
+	
+	//ID3D11Resource* pBackBuffer = nullptr;
+	wrl::ComPtr<ID3D11Resource> pBackBuffer;
+	pSwap->GetBuffer(0, __uuidof(ID3D11Resource), &pBackBuffer);
 	pDevice->CreateRenderTargetView(
-		pBackBuffer,
+		pBackBuffer.Get(),
 		nullptr,
 		&pTarget
 	);
@@ -51,22 +58,77 @@ void Graphics::EndFrame()
 	pSwap->Present(1u,  0u);
 }
 
+void Graphics::DrawTestTriangle()
+{
+	namespace wrl = Microsoft::WRL;
+
+	struct Vertex
+	{
+		float x;
+		float y;
+	};
+
+	const Vertex vertices[]
+	{
+		{0.0f, 0.5f},
+		{0.5f, -0.5f},
+		{-0.5f, -0.5f},
+	};
+
+	wrl::ComPtr<ID3D11Buffer> pVertexBuffer;
+	D3D11_BUFFER_DESC bd = {};
+	bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	bd.Usage = D3D11_USAGE_DEFAULT;
+	bd.CPUAccessFlags = 0u;
+	bd.MiscFlags = 0u;
+	bd.ByteWidth = sizeof(vertices);
+	bd.StructureByteStride = sizeof(Vertex);
+	D3D11_SUBRESOURCE_DATA sd = {};
+	sd.pSysMem = vertices;
+	pDevice->CreateBuffer(&bd, &sd, &pVertexBuffer);
+	const UINT stride = sizeof(Vertex);
+	const UINT offset = 0u;
+	pContext->IASetVertexBuffers(0u, 1u, pVertexBuffer.GetAddressOf(), &stride, &offset);
+
+	wrl::ComPtr<ID3D11PixelShader> pPixelShader;
+	wrl::ComPtr<ID3DBlob> pBlob;
+	D3DReadFileToBlob(L"PixelShader.cso", &pBlob);
+	pDevice->CreatePixelShader(pBlob->GetBufferPointer(), pBlob->GetBufferSize(), nullptr, &pPixelShader);
+	pContext->PSSetShader(pPixelShader.Get(), nullptr, 0u);
+
+	wrl::ComPtr<ID3D11VertexShader> pVertexShader;
+	D3DReadFileToBlob(L"VertexShader.cso", &pBlob);
+	pDevice->CreateVertexShader(pBlob->GetBufferPointer(), pBlob->GetBufferSize(), nullptr, &pVertexShader);
+	pContext->VSSetShader(pVertexShader.Get(), nullptr, 0u);
+
+	wrl::ComPtr<ID3D11InputLayout> pInputLayout;
+	const D3D11_INPUT_ELEMENT_DESC ied[] =
+	{
+		{"Position", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0},
+	};
+	pDevice->CreateInputLayout(
+		ied, (UINT)std::size(ied),
+		pBlob->GetBufferPointer(),
+		pBlob->GetBufferSize(),
+		&pInputLayout
+	);
+
+	pContext->IASetInputLayout(pInputLayout.Get());
+
+	pContext->OMSetRenderTargets(1u, pTarget.GetAddressOf(), nullptr);
+
+	D3D11_VIEWPORT vp;
+	vp.Width = 800;
+	vp.Height = 600;
+	vp.MinDepth = 0;
+	vp.MaxDepth = 1;
+	vp.TopLeftX = 0;
+	vp.TopLeftY = 0;
+	pContext->RSSetViewports(1u, &vp);
+
+	pContext->Draw(std::size(vertices), 0u);
+}
+
 Graphics::~Graphics()
 {
-	if (pDevice != nullptr)
-	{
-		pDevice->Release();
-	}
-	if (pSwap != nullptr)
-	{
-		pSwap->Release();
-	}
-	if (pContext != nullptr)
-	{
-		pContext->Release();
-	}
-	if (pTarget != nullptr)
-	{
-		pTarget->Release();
-	}
 }
--- hw3d/hw3d/Graphics.h
+++ hw3d/hw3d/Graphics.h
@@ -1,5 +1,14 @@
 #pragma once
 #include <d3d11.h>
+#include <wrl.h>
+
+namespace wrl = Microsoft::WRL;                                                             
+
+namespace Bind
+{
+	class Bindable;
+	class RenderTarget;
+}
 
 class Graphics
 {
@@ -12,12 +21,18 @@ public:
 	void ClearBuffer(float red, float green, float blue) noexcept
 	{
 		const float color[] = { red, green, blue, 1.0f };
-		pContext->ClearRenderTargetView(pTarget, color);
+		pContext->ClearRenderTargetView(pTarget.Get(), color);
 	}
+	void DrawTestTriangle();
 	~Graphics();
 private:
-	ID3D11Device* pDevice = nullptr;
+	wrl::ComPtr<ID3D11Device> pDevice;
+	wrl::ComPtr<IDXGISwapChain> pSwap;
+	wrl::ComPtr<ID3D11DeviceContext> pContext;
+	wrl::ComPtr<ID3D11RenderTargetView> pTarget;
+
+	/*ID3D11Device* pDevice = nullptr;
 	IDXGISwapChain* pSwap = nullptr;
 	ID3D11DeviceContext* pContext = nullptr;
-	ID3D11RenderTargetView* pTarget = nullptr;
+	ID3D11RenderTargetView* pTarget = nullptr;*/
 };
